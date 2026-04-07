58626d8d570efb61d133b16c56a83e8f8f24d919
PiotrDuma <piotrduma2014@gmail.com>
2026-04-07 16:11:08 +0200

    add selenium singleton webdriver factory

---
+++ src/main/java/com/github/PiotrDuma/webdriver/SingletonWebDriverFactory.java
@@ -0,0 +1,55 @@
+package com.github.PiotrDuma.webdriver;
+
+import com.github.PiotrDuma.utils.PropertyReader.PropertyReader;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SingletonWebDriverFactory {
+
+  private static final Logger logger = LoggerFactory.getLogger(SingletonWebDriverFactory.class);
+  private static final String LOG_INFO = "Init WebDriver: %s";
+  private static final String SYSTEM_BROWSER_PROPERTY = "browser";
+  private static final ThreadLocal<WebDriver> webDriver = new ThreadLocal<>();
+
+  private SingletonWebDriverFactory() {
+  }
+
+  // TODO: extend factory with another browsers
+  public static WebDriver getWebDriver() {
+    if (webDriver.get() == null) {
+      switch (getBrowserPropertyValue()) {
+        case "firefox" -> {
+          logger.info(String.format(LOG_INFO, "Firefox WebDriver"));
+          WebDriverManager.firefoxdriver().setup();
+          webDriver.set(new FirefoxDriver());
+        }
+        default -> {
+          logger.warn(String.format(LOG_INFO, "DEFAULT DRIVER: Firefox WebDriver"));
+          WebDriverManager.firefoxdriver().setup();
+          webDriver.set(new FirefoxDriver());
+        }
+      }
+      webDriver.get().manage().window().maximize();
+    }
+    return webDriver.get();
+  }
+
+  public static void closeDriver() {
+    WebDriver driver = webDriver.get();
+    if (driver != null) {
+      try {
+        logger.info("Terminate WebDriver");
+        driver.quit();
+      } finally {
+        webDriver.remove();
+      }
+    }
+  }
+
+  private static String getBrowserPropertyValue() {
+    return PropertyReader.getProperty(SYSTEM_BROWSER_PROPERTY).toLowerCase();
+  }
+}
