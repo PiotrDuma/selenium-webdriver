c5c6fa2aea09fd69190b898d81f99c71058fbb17
PiotrDuma <piotrduma2014@gmail.com>
2026-04-14 15:06:38 +0200

    add maven config to invoke testng tests

--- src/test/java/com/github/PiotrDuma/tests/LoginAndSendEmailSuccessTest.java
+++ src/test/java/com/github/PiotrDuma/tests/LoginAndSendEmailSuccessTest.java
@@ -69,6 +69,7 @@ public class LoginAndSendEmailSuccessTest {
         .isEqualTo(message);
   }
 
+
   @DataProvider(name = "credentials")
   public Object[][] getCredentials() {
     log.info("Access credentials data");
